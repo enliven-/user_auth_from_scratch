f9d010e25eaee4a545c499c2f96fbe2012af24a6
Viksit Arora <viksit@mit.edu>
2016-01-12 17:13:18 +0530

    installs bcrypt gem; adds has_secure_password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ActiveRecord::Base
             format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
   before_save { self.email = email.downcase }
-  
+
+  has_secure_password
 end
